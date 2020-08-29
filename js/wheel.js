9ff2163b6de681e366f91c79ba34ba5bcf8273ad
NFSS10 <nuno_nfss96@hotmail.com>
2020-08-29 22:57:38 +0100

    Turn wheel logic

---
+++ js/wheel.js
@@ -0,0 +1,43 @@
+function updateSlotsOrder(restaurants, restaurantNamesElements, timesUpdated) {
+    restaurantNamesElements.forEach((element, index) => {
+        const restaurantIndex = (timesUpdated - index + restaurants.length) % restaurants.length;
+        element.innerHTML = restaurants[restaurantIndex];
+    });
+}
+
+function wheelStep(topValue, velocity, timesUpdated, restaurants, restaurantNamesElements) {
+    setTimeout(() => {
+        if (wheel.offsetTop >= 0) {
+            topValue = -459;
+            timesUpdated++;
+
+            updateSlotsOrder(restaurants, restaurantNamesElements, timesUpdated);
+        }
+        else {
+            topValue = wheel.offsetTop + velocity;
+        }
+
+        wheel.style.top = `${topValue}px`;
+
+        console.log(velocity);
+        if (velocity > 0) {
+            velocity -= 1;
+            wheelStep(topValue, velocity, timesUpdated, restaurants, restaurantNamesElements);
+        }
+    }, 0);
+}
+
+function turnWheel(wheel, restaurants) {
+    let velocity = 300;
+
+    const restaurantNamesElements = [];
+    for (let i = 0; i < wheel.children.length; i++) {
+        restaurantNamesElements.push(wheel.children[i].getElementsByClassName("restaurant-name")[0]);
+    }
+
+    let topValue = -459;
+    let timesUpdated = 0;
+    updateSlotsOrder(restaurants, restaurantNamesElements, timesUpdated);
+
+    wheelStep(topValue, velocity, timesUpdated, restaurants, restaurantNamesElements);
+}
